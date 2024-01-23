1c0400b1f94dd976664c66042060537be91ea26d
Noureldin Mohamed <50280690+Noureldiiin@users.noreply.github.com>
2024-01-23 14:28:25 +0200

    Update get_patients.php

--- get_patients.php
+++ get_patients.php
@@ -8,7 +8,7 @@ $password = 'YdfLiJdJpb';
 $conn = new mysqli($host, $username, $password, $database, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connectionnnnnnnnnnnnnnn failed: " . $conn->connect_error);
     error_log('wdws');
 }
 
